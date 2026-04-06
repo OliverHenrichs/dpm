989b1714e839535ba67e4a4957cbbd7cc45761da
Oliver Henrichs <ohenrichs@gmail.com>
2026-04-06 19:52:23 +0200

    add splash screen fade-out

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,14 @@
 import "./i18n";
 import { ThemeProvider } from "@/src/common/components/ThemeContext";
 import DrawerNavigator from "@/src/common/components/DrawerNavigator";
+import * as SplashScreen from "expo-splash-screen";
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
   return (
